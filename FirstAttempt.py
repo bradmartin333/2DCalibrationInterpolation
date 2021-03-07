6be37591324319f7dc8c5a9dc52d6e6fad3bd6da
BradDesktop <brad.martin@xdisplay.com>
2021-03-07 17:09:22 -0500

    python is cool

--- FirstAttempt.py
+++ FirstAttempt.py
@@ -3,22 +3,26 @@ import numpy as np
 from scipy.interpolate import griddata
 
 # data coordinates and values
-x = [10,10,250,250,490,490,10,250,490]
-y = [10,490,10,490,490,10,250,250,250]
-z = [-1.001,-1.002,-1,-1,-1.003,-1.001,-0.995,-1.00,-1.001]
+x = [10,10,50,50]
+y = [10,50,10,50]
+z = [-1.001,-1.002,-1,-1.003]
 
 # target grid to interpolate to
-xi = yi = np.arange(0,500,5)
+xi = np.arange(0,800,5)
+yi = np.arange(0,500,5)
 xi,yi = np.meshgrid(xi,yi)
 
 # interpolate
 zi = griddata((x,y),z,(xi,yi),method='cubic')
+#zi = np.rot90(zi)
+zi = np.nan_to_num(zi)
+np.savetxt('output.tsv', zi, delimiter='\t', fmt='%1.3f')
 
-# plot
-fig = plt.figure()
-ax = fig.add_subplot(111)
-plt.contourf(xi,yi,zi,np.arange(min(z),max(z),0.00001))
-plt.plot(x,y,'k.')
-plt.xlabel('x',fontsize=16)
-plt.ylabel('y',fontsize=16)
-plt.show()
+# # plot
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# plt.contourf(xi,yi,zi,np.arange(min(z),max(z),0.0001))
+# plt.plot(x,y,'k.')
+# plt.xlabel('x',fontsize=16)
+# plt.ylabel('y',fontsize=16)
+# plt.show()
