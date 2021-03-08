78593772e188572126eb189c8a10a72c8c14ec9b
BradDesktop <brad.martin@xdisplay.com>
2021-03-08 09:46:30 -0500

    add a col to satisfy A3200

---
+++ AddColumn.py
@@ -0,0 +1,5 @@
+with open('output.txt', 'r') as dataIn:
+    with open('outputAddedCol.txt', 'w') as dataOut:
+        for line in dataIn:
+            line = line.rstrip('\n') + '\t0.000'
+            print(line, file=dataOut)
