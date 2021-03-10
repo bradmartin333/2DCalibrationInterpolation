387bb8c5ae15dfae1728525b94231697f93d9c2f
BradChroma <brad.martin@xdisplay.com>
2021-03-09 20:47:25 -0500

    average X and Y interpolation output

--- Test/Form1.cs
+++ Test/Form1.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using RDotNet;
 
 namespace Test
 {
@@ -16,17 +17,17 @@ namespace Test
         public Form1()
         {
             InitializeComponent();
-            _Steps = new Point((_Max.X - _Min.X) / _Increment, (_Max.Y - _Min.Y) / _Increment);
             LoadPositons();
             CalculateDeltas();
             Interpolate();
         }
 
-        private static int _Increment = 5;
         private Point _Min = new Point(0, 0);
         private Point _Max = new Point(500, 500);
-        private Point _Steps;
-        private double[,] _Positions = new double[5, 4];
+        private List<double> _X = new List<double>();
+        private List<double> _Y = new List<double>();
+        private List<double> _Z = new List<double>();
+        private List<double> _Zo = new List<double>();
 
         private void LoadPositons()
         {
@@ -36,10 +37,10 @@ namespace Test
             while ((line = file.ReadLine()) != null)
             {
                 string[] data = line.Split(',');
-                _Positions[idx, 0] = double.Parse(data[0]);
-                _Positions[idx, 1] = double.Parse(data[1]);
-                _Positions[idx, 2] = double.Parse(data[2]);
-                _Positions[idx, 3] = double.Parse(data[5]);
+                _X.Add(double.Parse(data[0]));
+                _Y.Add(double.Parse(data[1]));
+                _Z.Add(double.Parse(data[2]));
+                _Zo.Add(double.Parse(data[5]));
                 idx++;
             }
             file.Close();
@@ -47,25 +48,35 @@ namespace Test
 
         private void CalculateDeltas()
         {
-            double MinZ = 1e10;
-            double MinZo = 0;
-            for (int i = 0; i < 5; i++)
+            double minZ = _Z.Min();
+            double minZo = _Zo.Min();
+            for (int i = 0; i<_X.Count; i++)
             {
-                if (_Positions[i, 2] < MinZ)
-                    MinZ = _Positions[i, 2];
-                if (_Positions[i, 3] < MinZo)
-                    MinZo = _Positions[i, 3];
-            }
-            for (int i = 0; i < 5; i++)
-            {
-                _Positions[i, 2] -= MinZ;
-                _Positions[i, 3] -= MinZo;
+                _Z[i] -= minZ;
+                _Zo[i] -= minZo;
             }
         }
 
         private void Interpolate()
         {
-           
+            MathNet.Numerics.Interpolation.IInterpolation Xinterpolation = MathNet.Numerics.Interpolate.Linear(_X, _Zo);
+            MathNet.Numerics.Interpolation.IInterpolation Yinterpolation = MathNet.Numerics.Interpolate.Linear(_Y, _Zo);
+            using (StreamWriter sw = File.CreateText("CSoutput.txt"))
+            {
+                for (int i = _Min.X; i <= _Max.X; i += 5)
+                {
+                    for (int j = _Min.Y; j < _Max.Y; j += 5)
+                    {
+                        double Xvalue = Xinterpolation.Interpolate(i);
+                        if (Xvalue == double.NegativeInfinity)
+                            Xvalue = 0;
+                        double Yvalue = Yinterpolation.Interpolate(j);
+                        if (Yvalue == double.NegativeInfinity)
+                            Yvalue = 0;
+                        sw.WriteLine(string.Format("{0}{1}{2}{3}{4}", i, '\t', j, '\t', (Xvalue + Yvalue) / 2));
+                    }
+                }
+            }
         }
     }
 }
