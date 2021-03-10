5d40d12b3455333c55f8f403a8274e62535d7299
BradDesktop <brad.martin@xdisplay.com>
2021-03-10 10:56:39 -0500

    it works!

--- GSL/Project1/Source.cpp
+++ GSL/Project1/Source.cpp
@@ -1,15 +1,29 @@
 #include <iostream> 
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_interp2d.h>
 #include <gsl/gsl_spline2d.h>
 
+/*
+	./interpx.exe {1} {2} {3} {4} {5} {6} {7}
+	{0} = xRange of measurements
+	{1} = yRange of measurements
+	{2} = X coordinate of interest
+	{3} = Y coordinate of interest
+	{4] = SW z measurement 
+	{5] = NW z measurement
+	{6] = NE z measurement
+	{7] = SE z measurement
+*/
+
 int main(int argc, char** argv) 
 {
 	const gsl_interp2d_type* T = gsl_interp2d_bilinear;
-	const size_t N = 100;			  /* number of points to interpolate */
+	const double xRange = atof(argv[1]);
+	const double yRange = atof(argv[2]);
+	const size_t Nx = (size_t)(xRange); /* number of points to interpolate */
+	const size_t Ny = (size_t)(yRange);
 	const double xa[] = { 0.0, 1.0 }; /* define unit square */
 	const double ya[] = { 0.0, 1.0 };
 	const size_t nx = sizeof(xa) / sizeof(double); /* x grid points */
@@ -18,42 +32,25 @@ int main(int argc, char** argv)
 	gsl_spline2d* spline = gsl_spline2d_alloc(T, nx, ny);
 	gsl_interp_accel *xacc = gsl_interp_accel_alloc();
 	gsl_interp_accel* yacc = gsl_interp_accel_alloc();
-	double i, j;
 
 	/* set z grid values */
-	gsl_spline2d_set(spline, za, 0, 0, 0.0);
-	gsl_spline2d_set(spline, za, 0, 1, 1.0);
-	gsl_spline2d_set(spline, za, 1, 1, 0.5);
-	gsl_spline2d_set(spline, za, 1, 0, 1.0);
+	gsl_spline2d_set(spline, za, 0, 0, atof(argv[5]));
+	gsl_spline2d_set(spline, za, 0, 1, atof(argv[6]));
+	gsl_spline2d_set(spline, za, 1, 1, atof(argv[7]));
+	gsl_spline2d_set(spline, za, 1, 0, atof(argv[8]));
 
 	/* initialize interpolation */
 	gsl_spline2d_init(spline, xa, ya, za, nx, ny);
 
-	/* interpolate N values in x and y and print out grid for plotting */
-	for (i = 0; i < N; ++i)
-	{
-		double xi = i / (N - 1.0);
-
-		for (j = 0; j < N; ++j)
-		{
-			double yj = j / (N - 1.0);
-			double zij = gsl_spline2d_eval(spline, xi, yj, xacc, yacc);
-
-			printf("%f %f %f\n", xi, yj, zij);
-		}
-		printf("\n");
-	}
-
+	/* interpolate and print */
+	double xi = atof(argv[3]) / (Nx - 1.0);
+	double yj = atof(argv[4]) / (Ny - 1.0);
+	double zij = gsl_spline2d_eval(spline, xi, yj, xacc, yacc);
+	printf("\n%f\n", zij);
 	gsl_spline2d_free(spline);
 	gsl_interp_accel_free(xacc);
 	gsl_interp_accel_free(yacc);
 	free(za);
-	
-	std::cout << "You have entered " << argc
-		<< " arguments:" << "\n";
-
-	for (int i = 0; i < argc; ++i)
-		std::cout << argv[i] << "\n";
 
 	return 0;
 }
