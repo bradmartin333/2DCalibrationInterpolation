5b936812216499f5afa60a389d74024642cedc6a
BradChroma <brad.martin@xdisplay.com>
2021-03-10 19:49:31 -0500

    attempt heatmaps

--- Test/Form1.Designer.cs
+++ Test/Form1.Designer.cs
@@ -42,16 +42,22 @@ namespace Test
             this.label2 = new System.Windows.Forms.Label();
             this.lblZo = new System.Windows.Forms.Label();
             this.lblZoOffset = new System.Windows.Forms.Label();
+            this.pbxZ = new System.Windows.Forms.PictureBox();
+            this.pbxZo = new System.Windows.Forms.PictureBox();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numX)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxZ)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxZo)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel1.Controls.Add(this.numX, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.numY, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.lblZ, 1, 2);
@@ -64,6 +70,8 @@ namespace Test
             this.tableLayoutPanel1.Controls.Add(this.label2, 0, 5);
             this.tableLayoutPanel1.Controls.Add(this.lblZo, 1, 4);
             this.tableLayoutPanel1.Controls.Add(this.lblZoOffset, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.pbxZ, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pbxZo, 3, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -74,16 +82,16 @@ namespace Test
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(275, 215);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(647, 227);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // numX
             // 
             this.numX.DecimalPlaces = 3;
             this.numX.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.numX.Location = new System.Drawing.Point(140, 3);
+            this.numX.Location = new System.Drawing.Point(110, 3);
             this.numX.Name = "numX";
-            this.numX.Size = new System.Drawing.Size(132, 23);
+            this.numX.Size = new System.Drawing.Size(101, 23);
             this.numX.TabIndex = 0;
             this.numX.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.numX.ValueChanged += new System.EventHandler(this.num_Changed);
@@ -92,9 +100,9 @@ namespace Test
             // 
             this.numY.DecimalPlaces = 3;
             this.numY.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.numY.Location = new System.Drawing.Point(140, 38);
+            this.numY.Location = new System.Drawing.Point(110, 40);
             this.numY.Name = "numY";
-            this.numY.Size = new System.Drawing.Size(132, 23);
+            this.numY.Size = new System.Drawing.Size(101, 23);
             this.numY.TabIndex = 1;
             this.numY.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.numY.ValueChanged += new System.EventHandler(this.num_Changed);
@@ -103,9 +111,9 @@ namespace Test
             // 
             this.lblZ.AutoSize = true;
             this.lblZ.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblZ.Location = new System.Drawing.Point(140, 70);
+            this.lblZ.Location = new System.Drawing.Point(110, 74);
             this.lblZ.Name = "lblZ";
-            this.lblZ.Size = new System.Drawing.Size(132, 35);
+            this.lblZ.Size = new System.Drawing.Size(101, 37);
             this.lblZ.TabIndex = 2;
             this.lblZ.Text = "label1";
             this.lblZ.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -114,9 +122,9 @@ namespace Test
             // 
             this.lblZOffset.AutoSize = true;
             this.lblZOffset.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblZOffset.Location = new System.Drawing.Point(140, 105);
+            this.lblZOffset.Location = new System.Drawing.Point(110, 111);
             this.lblZOffset.Name = "lblZOffset";
-            this.lblZOffset.Size = new System.Drawing.Size(132, 35);
+            this.lblZOffset.Size = new System.Drawing.Size(101, 37);
             this.lblZOffset.TabIndex = 3;
             this.lblZOffset.Text = "label2";
             this.lblZOffset.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -127,7 +135,7 @@ namespace Test
             this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label3.Location = new System.Drawing.Point(3, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(131, 35);
+            this.label3.Size = new System.Drawing.Size(101, 37);
             this.label3.TabIndex = 4;
             this.label3.Text = "X Position";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -136,9 +144,9 @@ namespace Test
             // 
             this.label4.AutoSize = true;
             this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label4.Location = new System.Drawing.Point(3, 35);
+            this.label4.Location = new System.Drawing.Point(3, 37);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(131, 35);
+            this.label4.Size = new System.Drawing.Size(101, 37);
             this.label4.TabIndex = 5;
             this.label4.Text = "Y Position";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -147,9 +155,9 @@ namespace Test
             // 
             this.label5.AutoSize = true;
             this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label5.Location = new System.Drawing.Point(3, 70);
+            this.label5.Location = new System.Drawing.Point(3, 74);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(131, 35);
+            this.label5.Size = new System.Drawing.Size(101, 37);
             this.label5.TabIndex = 6;
             this.label5.Text = "Z Position";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -158,9 +166,9 @@ namespace Test
             // 
             this.label6.AutoSize = true;
             this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label6.Location = new System.Drawing.Point(3, 105);
+            this.label6.Location = new System.Drawing.Point(3, 111);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(131, 35);
+            this.label6.Size = new System.Drawing.Size(101, 37);
             this.label6.TabIndex = 7;
             this.label6.Text = "Z Offset";
             this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -169,9 +177,9 @@ namespace Test
             // 
             this.label1.AutoSize = true;
             this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label1.Location = new System.Drawing.Point(3, 140);
+            this.label1.Location = new System.Drawing.Point(3, 148);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(131, 35);
+            this.label1.Size = new System.Drawing.Size(101, 37);
             this.label1.TabIndex = 8;
             this.label1.Text = "Zo Position";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -180,9 +188,9 @@ namespace Test
             // 
             this.label2.AutoSize = true;
             this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label2.Location = new System.Drawing.Point(3, 175);
+            this.label2.Location = new System.Drawing.Point(3, 185);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(131, 40);
+            this.label2.Size = new System.Drawing.Size(101, 42);
             this.label2.TabIndex = 9;
             this.label2.Text = "Z Position";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -191,9 +199,9 @@ namespace Test
             // 
             this.lblZo.AutoSize = true;
             this.lblZo.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblZo.Location = new System.Drawing.Point(140, 140);
+            this.lblZo.Location = new System.Drawing.Point(110, 148);
             this.lblZo.Name = "lblZo";
-            this.lblZo.Size = new System.Drawing.Size(132, 35);
+            this.lblZo.Size = new System.Drawing.Size(101, 37);
             this.lblZo.TabIndex = 10;
             this.lblZo.Text = "label7";
             this.lblZo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -202,18 +210,42 @@ namespace Test
             // 
             this.lblZoOffset.AutoSize = true;
             this.lblZoOffset.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblZoOffset.Location = new System.Drawing.Point(140, 175);
+            this.lblZoOffset.Location = new System.Drawing.Point(110, 185);
             this.lblZoOffset.Name = "lblZoOffset";
-            this.lblZoOffset.Size = new System.Drawing.Size(132, 40);
+            this.lblZoOffset.Size = new System.Drawing.Size(101, 42);
             this.lblZoOffset.TabIndex = 11;
             this.lblZoOffset.Text = "label8";
             this.lblZoOffset.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pbxZ
+            // 
+            this.pbxZ.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbxZ.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbxZ.Location = new System.Drawing.Point(217, 3);
+            this.pbxZ.Name = "pbxZ";
+            this.tableLayoutPanel1.SetRowSpan(this.pbxZ, 6);
+            this.pbxZ.Size = new System.Drawing.Size(209, 221);
+            this.pbxZ.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxZ.TabIndex = 12;
+            this.pbxZ.TabStop = false;
+            // 
+            // pbxZo
+            // 
+            this.pbxZo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbxZo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbxZo.Location = new System.Drawing.Point(432, 3);
+            this.pbxZo.Name = "pbxZo";
+            this.tableLayoutPanel1.SetRowSpan(this.pbxZo, 6);
+            this.pbxZo.Size = new System.Drawing.Size(212, 221);
+            this.pbxZo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxZo.TabIndex = 13;
+            this.pbxZo.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(275, 215);
+            this.ClientSize = new System.Drawing.Size(647, 227);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "interpx";
@@ -221,6 +253,8 @@ namespace Test
             this.tableLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numX)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxZ)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxZo)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -240,6 +274,8 @@ namespace Test
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label lblZo;
         private System.Windows.Forms.Label lblZoOffset;
+        private System.Windows.Forms.PictureBox pbxZ;
+        private System.Windows.Forms.PictureBox pbxZo;
     }
 }
 
--- Test/Form1.cs
+++ Test/Form1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
@@ -37,8 +39,10 @@ namespace Test
             // Don't let use go into "uncharted" territory
             numX.Minimum = (decimal)_X.Min();
             numX.Maximum = (decimal)_X.Max();
+            numX.Value = (decimal)(_X.Min() + _X.Max()) / 2;
             numY.Minimum = (decimal)_Y.Min();
             numY.Maximum = (decimal)_Y.Max();
+            numY.Value = (decimal)(_Y.Min() + _Y.Max()) / 2;
 
             // Length of _Z and _Zo need to equal the length of _X times the length of _Y
             // len(_Z) == len(_Zo) == len(_X) * len(_Y)
@@ -48,6 +52,7 @@ namespace Test
             alglib.spline2dbuildbicubicv(_X.ToArray(), _X.Count(), _Y.ToArray(), _Y.Count(), _Zo.ToArray(), 1, out ZoSpline);
 
             _Loaded = true;
+            MakePlots();
             updateAll();
         }
 
@@ -75,6 +80,88 @@ namespace Test
             lblZOffset.Text = vZOff.ToString();
             lblZo.Text = vZo.ToString();
             lblZoOffset.Text = vZoOff.ToString();
+
+            MakeCursor();
+        }
+
+        private void MakeCursor()
+        {
+            Bitmap bmpZ = (Bitmap)pbxZ.BackgroundImage.Clone();
+            using (Graphics G = Graphics.FromImage(bmpZ))
+            {
+                using (SolidBrush brush = new SolidBrush(Color.HotPink))
+                    G.FillRectangle(brush, (int)((double)numX.Value - bmpZ.Width/100 - _X.Min()), 
+                                           (int)((double)numY.Value - bmpZ.Height/100 - _Y.Min()),
+                                           bmpZ.Width / 50, bmpZ.Height / 50);
+            }
+            pbxZ.Image = bmpZ;
+
+            Bitmap bmpZo = (Bitmap)pbxZo.BackgroundImage.Clone();
+            using (Graphics G = Graphics.FromImage(bmpZo))
+            {
+                using (SolidBrush brush = new SolidBrush(Color.HotPink))
+                    G.FillRectangle(brush, (int)((double)numX.Value - bmpZ.Width / 100 - _X.Min()),
+                                           (int)((double)numY.Value - bmpZ.Height / 100 - _Y.Min()),
+                                           bmpZ.Width / 50, bmpZ.Height / 50);
+            }
+            pbxZo.Image = bmpZo;
+        }
+
+        private void MakePlots()
+        {
+            int[] xs = Enumerable.Range((int)_X.Min(), (int)(_X.Max() - _X.Min())).ToArray();
+            int[] ys = Enumerable.Range((int)_Y.Min(), (int)(_Y.Max() - _Y.Min())).ToArray();
+
+            double[,] Zs = new double[ys.Length, xs.Length];
+            double[,] Zos = new double[ys.Length, xs.Length];
+            for (int i = 0; i < ys.Length; i++)
+            {
+                for (int j = 0; j < xs.Length; j++)
+                {
+                    Zs[i, j] = alglib.spline2dcalc(ZSpline, j, i);
+                    Zos[i, j] = alglib.spline2dcalc(ZoSpline, j, i);
+                }
+            }
+
+            Bitmap bmpZ = new Bitmap(xs.Length, ys.Length);
+            using (Graphics G = Graphics.FromImage(bmpZ))
+            {
+                float w = 1f * xs.Length / Zs.GetLength(0);
+                float h = 1f * ys.Length / Zs.GetLength(1);
+                for (int x = 0; x < Zs.GetLength(0); x++)
+                    for (int y = 0; y < Zs.GetLength(1); y++)
+                    {
+                        int lux = (int)((Zs[x, y] - _Z.Max()) / (_Z.Max() - _Z.Min()) * 255);
+                        if (lux < 0)
+                            lux = 0;
+                        if (lux > 255)
+                            lux = 255;
+                        using (SolidBrush brush = new SolidBrush(Color.FromArgb(255, lux, lux, lux)))
+                            G.FillRectangle(brush, x * w, y * h, w, h);
+                    }
+            }
+            bmpZ.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            pbxZ.BackgroundImage = bmpZ;
+
+            Bitmap bmpZo = new Bitmap(xs.Length, ys.Length);
+            using (Graphics G = Graphics.FromImage(bmpZo))
+            {
+                float w = 1f * xs.Length / Zos.GetLength(0);
+                float h = 1f * ys.Length / Zos.GetLength(1);
+                for (int x = 0; x < Zos.GetLength(0); x++)
+                    for (int y = 0; y < Zos.GetLength(1); y++)
+                    {
+                        int lux = (int)((Zos[x, y] - _Zo.Max()) / (_Zo.Max() - _Zo.Min()) * 255);
+                        if (lux < 0)
+                            lux = 0;
+                        if (lux > 255)
+                            lux = 255;
+                        using (SolidBrush brush = new SolidBrush(Color.FromArgb(255, lux, lux, lux)))
+                            G.FillRectangle(brush, x * w, y * h, w, h);
+                    }
+            }
+            bmpZo.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            pbxZo.BackgroundImage = bmpZo;
         }
     }
 }
