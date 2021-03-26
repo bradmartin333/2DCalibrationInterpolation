ecc4374de242f42b6d24cd23d1908bbbd9df7427
BradDesktop <brad.martin@xdisplay.com>
2021-03-26 09:13:24 -0400

    create the hopefully final project

---
+++ PolyInterp/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PolyInterp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadPositons();
+        }
+
+        private List<double> _X = new List<double>(), _Y = new List<double>(), _Zo = new List<double>();
+
+        private void LoadPositons()
+        {
+            string line;
+            StreamReader file = new StreamReader("PositionMemory.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] data = line.Split(',');
+                if (!_X.Contains(double.Parse(data[0]))) // Easy way to limit _X to grid values
+                    _X.Add(double.Parse(data[0]));
+                if (!_Y.Contains(double.Parse(data[1]))) // Easy way to limit _Y to grid values
+                    _Y.Add(double.Parse(data[1]));
+                _Zo.Add(double.Parse(data[5]));
+            }
+            file.Close();
+        }
+    }
+}
---
+++ PolyInterp/Interpolator.cs
@@ -0,0 +1,42 @@
+﻿namespace XferPrint.Utility
+{
+    public class Interpolator
+    {
+        public double SW { get; set; }
+        public double SE { get; set; }
+        public double NW { get; set; }
+        public double NE { get; set; }
+        public double X1 { get; set; }
+        public double X2 { get; set; }
+        public double Y1 { get; set; }
+        public double Y2 { get; set; }
+
+        public Interpolator(double sw, double se, double nw, double ne, double x1, double x2, double y1, double y2)
+        {
+            SW = sw;
+            SE = se;
+            NW = nw;
+            NE = ne;
+            X1 = x1;
+            X2 = x2;
+            Y1 = y1;
+            Y2 = y2;
+        }
+
+        public double Get(double x, double y)
+        {
+            double x2x1 = X2 - X1;
+            double y2y1 = Y2 - Y1;
+            double x2x = X2 - x;
+            double y2y = Y2 - y;
+            double yy1 = y - Y1;
+            double xx1 = x - X1;
+            return 1.0 / (x2x1 * y2y1) * (
+                SW * x2x * y2y +
+                SE * xx1 * y2y +
+                NW * x2x * yy1 +
+                NE * xx1 * yy1
+            );
+        }
+    }
+}
