b10416633de8bad2b948cfdbe643a963fce82e71
BradDesktop <brad.martin@xdisplay.com>
2021-06-18 10:24:12 -0400

    rotated scatter

--- SPOT/Test.Designer.cs
+++ SPOT/Test.Designer.cs
@@ -29,12 +29,54 @@ namespace SPOT
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.scatterPlotView = new OxyPlot.WindowsForms.PlotView();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.scatterPlotView, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(641, 549);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // scatterPlotView
+            // 
+            this.scatterPlotView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.scatterPlotView.Location = new System.Drawing.Point(3, 3);
+            this.scatterPlotView.Name = "scatterPlotView";
+            this.scatterPlotView.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.scatterPlotView.Size = new System.Drawing.Size(635, 543);
+            this.scatterPlotView.TabIndex = 1;
+            this.scatterPlotView.Text = "plotView2";
+            this.scatterPlotView.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.scatterPlotView.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.scatterPlotView.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // Test
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(641, 549);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Test";
             this.Text = "Test";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private OxyPlot.WindowsForms.PlotView scatterPlotView;
     }
 }
--- SPOT/Test.cs
+++ SPOT/Test.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +16,66 @@ namespace SPOT
 {
     public partial class Test : Form
     {
+        private static PointF SW = new PointF(-2.561f, -2.967f);
+        private static PointF SE = new PointF(4.095f, -2.959f);
+        private static PointF NW, NE;
+        private static PointF Pitch = new PointF(0.4f, 0.4f);
+        private static PointF Num = new PointF(40, 40);
+        private static int Step = 25; // Microns
+        private static double[,] Data;
+        private static int NumX = 100;
+        private static int NumY = 100;
+        private static double Angle;
+
         public Test()
         {
             InitializeComponent();
+            Angle = Math.Atan2(SE.Y - SW.Y, SE.X - SW.X) * 180 / Math.PI;
+            Data = new double[NumX, NumY];
+
+            var model = new PlotModel { Title = "ScatterSeries" };
+            scatterPlotView.Model = model;
+
+            NW = new PointF(SW.X, SW.Y + Num.Y * Pitch.Y);
+            NE = new PointF(SE.X, SE.Y + Num.Y * Pitch.Y);
+            RotatePoint(ref NW);
+            RotatePoint(ref NE);
+
+            List<PointF> points = new List<PointF>() { SW, SE, NW, NE };
+            RectangleF rectangle = GetBoundingRect(points);
+
+            for (int i = 0; i < rectangle.Width * 1000; i += Step)
+            {
+                for (int j = 0; j < rectangle.Height * 1000; j += Step)
+                { 
+                    PointF scanCoord = new PointF(i, j);
+                    RotatePoint(ref scanCoord);
+                    Random random = new Random((i+1)%(j+1));
+                    var scatterSeries = new ScatterSeries()
+                    {
+                        MarkerSize = 1,
+                        MarkerStroke = OxyColor.FromRgb((byte)(random.NextDouble() * 255), (byte)(random.NextDouble() * 255), (byte)(random.NextDouble() * 255))
+                    };
+                    scatterSeries.Points.Add(new ScatterPoint(scanCoord.X, scanCoord.Y));
+                    scatterPlotView.Model.Series.Add(scatterSeries);
+                }
+            }
+        }
+
+        public static void RotatePoint(ref PointF point)
+        {
+            float x = (float)(point.X * Math.Cos(Math.PI / 180d * Angle) - point.Y * Math.Sin(Math.PI / 180d * Angle));
+            float y= (float)(point.X * Math.Sin(Math.PI / 180d * Angle) + point.Y * Math.Cos(Math.PI / 180d * Angle));
+            point = new PointF(x, y);
+        }
+
+        public RectangleF GetBoundingRect(List<PointF> points)
+        {
+            float minX = points.Min(p => p.X);
+            float minY = points.Min(p => p.Y);
+            float maxX = points.Max(p => p.X);
+            float maxY = points.Max(p => p.Y);
+            return new RectangleF(new PointF(minX, minY), new SizeF(maxX - minX, maxY - minY));
         }
     }
 }
